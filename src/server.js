80eb8221d9909aa4e2d46eb3db22c303bc18c76b
YuliyaRoslikova <julia.roslikov@gmail.com>
2024-06-04 10:38:39 +0300

    fixes

--- src/server.js
+++ src/server.js
@@ -11,13 +11,13 @@ export const setupServer = () => {
 
   app.use(cors());
 
-  // app.use(
-  //   pino({
-  //     transport: {
-  //       target: 'pino-pretty',
-  //     },
-  //   }),
-  // );
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
   app.get('/contacts', async (_req, res) => {
     try {
