0a75cd6b0d46c539f8012f4d9ca9cc1c3f918f98
YuliyaRoslikova <julia.roslikov@gmail.com>
2024-07-04 02:32:42 +0300

    send brevo mail

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -5,11 +5,11 @@ import { SMTP } from '../constants/index.js';
 const transporter = nodemailer.createTransport({
   host: env(SMTP.SMTP_HOST),
   port: Number(env(SMTP.SMTP_PORT)),
+  secure: false,
   auth: {
     user: env(SMTP.SMTP_USER),
     pass: env(SMTP.SMTP_PASSWORD),
   },
-  // from: env(SMTP.SMTP_USER),
 });
 
 export const sendEmail = async (options) => {
